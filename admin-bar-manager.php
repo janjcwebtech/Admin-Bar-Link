46acdc48c5e759188ba7d947f6789d70cd992bc3
janjcwebtech <jan@jcweb.tech>
2025-02-21 15:15:56 +0000

    Update admin-bar-manager.php

--- admin-bar-manager.php
+++ admin-bar-manager.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
  * Plugin Name: Admin Bar Link
- * Description: Adds a link to the Theme Editor (global CSS) in the WordPress admin bar and removes specific links like Comments and Rank Math.
+ * Description: Adds a link to the Theme Editor (global CSS) in the WordPress admin bar and removes specific links like Comments and Rank Math. And improves behaviour.
  * Version: 1.1
  * Author: JC
  */
@@ -11,15 +12,17 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class AdminBarLink {
-
-    public function __construct() {
+class AdminBarLink
+{
+    public function __construct()
+    {
         add_action('admin_bar_menu', [$this, 'add_theme_editor_link'], 100);
         add_action('admin_bar_menu', [$this, 'remove_unwanted_links'], 99);
         add_action('wp_enqueue_scripts', [$this, 'ensure_admin_bar_shows'], 1);
     }
 
-    public function add_theme_editor_link($wp_admin_bar) {
+    public function add_theme_editor_link($wp_admin_bar)
+    {
         // Add a new link to the admin bar
         $wp_admin_bar->add_node([
             'id'    => 'theme-editor-link',
@@ -28,7 +31,8 @@ class AdminBarLink {
         ]);
     }
 
-    public function remove_unwanted_links($wp_admin_bar) {
+    public function remove_unwanted_links($wp_admin_bar)
+    {
         // Remove Comments link
         $wp_admin_bar->remove_node('comments');
 
@@ -36,7 +40,8 @@ class AdminBarLink {
         $wp_admin_bar->remove_node('rank-math');
     }
 
-    public function ensure_admin_bar_shows() {
+    public function ensure_admin_bar_shows()
+    {
         if (is_user_logged_in() && current_user_can('edit_theme_options')) {
             show_admin_bar(true);
         }
